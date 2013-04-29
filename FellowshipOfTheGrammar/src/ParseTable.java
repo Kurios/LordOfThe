3049cd0fe0dc9c649e3ac45240415c594c322de5
Maxwell McRae <mmcrae7@gatech.edu>
2013-04-29 19:14:08 -0400

    still working on first adding for parse table
    i'm tired

--- FellowshipOfTheGrammar/src/ParseTable.java
+++ FellowshipOfTheGrammar/src/ParseTable.java
@@ -1,15 +1,32 @@
-import java.util.LinkedList;
+import java.util.*;
 
 
 public class ParseTable 
 {
+	LinkedList<GToken> reference=new LinkedList<GToken>();
+	LinkedList<HashMap<GToken,GRule>> table=new LinkedList<HashMap<GToken,GRule>>();
 	public ParseTable(LinkedList<GToken> tokens,LinkedList<GRule> rules)
 	{
+		for (GRule r:rules)
+		{
+			table.add(new HashMap());
+			reference.add(r.self);
+			if(!(r.tokenList==null))
+			{
+				for (GToken fir:r.tokenList.get(0).first)
+				{
+					//fir is now for all rules in follow.
+				}
+				
+				
+			}
+		}
+		/*
 		for (GToken t:tokens)
 		{
 			
 			
 		}
-		
+		*/
 	}
 }
