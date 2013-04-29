870aaca7a5e62ab13cab37d16ffe6b147bf41855
Maxwell McRae <mmcrae7@gatech.edu>
2013-04-29 19:35:35 -0400

    ParseTable coded, just needs to be tested.
    "Just"

--- FellowshipOfTheGrammar/src/ParseTable.java
+++ FellowshipOfTheGrammar/src/ParseTable.java
@@ -9,18 +9,25 @@ public class ParseTable
 	{
 		for (GRule r:rules)
 		{
-			table.add(new HashMap());
+			table.add(new HashMap<GToken,GRule>());
 			reference.add(r.self);
 			if(!(r.tokenList==null))
 			{
 				for (GToken fir:r.tokenList.get(0).first)
 				{
+					table.get(reference.indexOf(r.self)).put(fir, r);//this is stupid and i feel stupid
 					//fir is now for all rules in follow.
 				}
-				
+				for (GToken fol:r.tokenList.get(0).follow)
+				{
+					table.get(reference.indexOf(r.self)).put(fol, r);//this is stupid and i feel stupid
+					//fir is now for all rules in follow.
+				} 
 				
 			}
 		}
+		//whelp, now if you want a resulution at (a,b) you do
+		//table.get(reference.indexOf(a)).get(b); //which returns the rule you do
 		/*
 		for (GToken t:tokens)
 		{
