64fd0033e7ad0e6a0f83c51c74adf53dc745e91e
Maxwell McRae <mmcrae7@gatech.edu>
2013-04-29 21:22:18 -0400

    finished parsetable, just needs to be tested. again.
    ha

--- FellowshipOfTheGrammar/src/ParseTable.java
+++ FellowshipOfTheGrammar/src/ParseTable.java
@@ -5,24 +5,37 @@ public class ParseTable
 {
 	LinkedList<GToken> reference=new LinkedList<GToken>();
 	LinkedList<HashMap<GToken,GRule>> table=new LinkedList<HashMap<GToken,GRule>>();
-	public ParseTable(LinkedList<GToken> tokens,LinkedList<GRule> rules)
+	public ParseTable(LinkedList<GToken> tokens,LinkedList<GRule> rules,GToken epsilon)
 	{
-		for (GRule r:rules)
+		for (GRule r:rules) //for each rule in grammar
 		{
 			table.add(new HashMap<GToken,GRule>());
 			reference.add(r.self);
-			if(!(r.tokenList==null))
+			if(!(r.tokenList==null))//if it exists
 			{
-				for (GToken fir:r.tokenList.get(0).first)
-				{
-					table.get(reference.indexOf(r.self)).put(fir, r);//this is stupid and i feel stupid
+				for (GToken fir:r.tokenList.get(0).first)//grab the first set of the first token
+				{//this is inserted into r.self,fir on the rule section.
+					if(!fir.equals(epsilon))table.get(reference.indexOf(r.self)).put(fir, r);//if not add straight up
+					if(fir.equals(epsilon))//
+					{
+						//this is supposed to be for "if has the epsilon thinggy
+						//table.get(reference.indexOf(r.self)).put(fir, r);//this is stupid and i feel stupid
+						//for (GToken firt:r.tokenList.get(0).first)if(r.self.follow.contains(firt))table.get(reference.indexOf(r.self)).put(firt, r);
+						//the above line , since it found an epsilon result in r.self, actually uses
+						//the damn follow sets. so for all the things in the first set, it 
+						//here has to traverse the fol set of self for a match to tokenlist.get(0)
+						for (GToken foll:r.self.follow)table.get(reference.indexOf(r.self)).put(foll, r);
+						//and THIS line is when i realized i was being stupid and didn't need an if statment
+					}
 					//fir is now for all rules in follow.
 				}
+				/*
 				for (GToken fol:r.tokenList.get(0).follow)
 				{
 					table.get(reference.indexOf(r.self)).put(fol, r);//this is stupid and i feel stupid
 					//fir is now for all rules in follow.
 				} 
+				*/
 				
 			}
 		}
